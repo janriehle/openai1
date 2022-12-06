778724e55304daa377c7c065c70be4df237ed24b
janriehle <janriehle@gmail.com>
2022-12-06 11:14:09 -0300

    v8

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
